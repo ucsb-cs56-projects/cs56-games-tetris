92b5aec4cc2cde42ba864a41db348a4b5589207c
Wesley Peery <peery@umail.ucsb.edu>
2016-11-17 16:29:56 -0800

    grid added

--- src/edu/ucsb/cs56/projects/games/tetris/TetrisBoard.java
+++ src/edu/ucsb/cs56/projects/games/tetris/TetrisBoard.java
@@ -735,6 +735,7 @@ public class TetrisBoard extends JPanel implements ActionListener {
 		else{ // Add hollow square
 		    gr.setColor(Color.WHITE);
 		    gr.fillRect(20*col,20*row,20,20);
+		    gr.setColor(Color.BLACK);
 		    gr.drawRect(20*col,20*row,20,20);
 		    
 		}		
